1a51c6b1fa71645e0e90b16fa18f18ccbfb9ab40
darienz <darienf@gmail.com>
2019-09-29 17:27:27 -0400

    Added test button.

---
+++ Assets/Scripts/TestButtonBhavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+public class Dummy
+{
+    public int x;
+    public string stringythingy;
+}
+public class TestButtonBhavior : MonoBehaviour
+{
+
+    public Button testButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        testButton.onClick.AddListener(Test("newName"));
+
+        var t = new Dummy();
+
+        t.x = 5;
+        t.stringythingy = "wowweeee!";
+
+
+        var x = Newtonsoft.Json.JsonSerializer.Create();
+        var tr = new System.IO.StringWriter();
+        x.Serialize(tr, t);
+        var m = x.ToString();
+        Debug.Log(m);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    UnityEngine.Events.UnityAction Test(string a)
+    {
+        return () => testButton.GetComponentInChildren<Text>().text = a;
+
+
+    }
+}
